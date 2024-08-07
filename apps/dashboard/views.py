454c18f22fe36ca764185320409275b98318adb2
Matheus Granda <matheus.granda@gmail.com>
2024-08-06 22:55:05 -0300

    insert month sells condition

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from produtos.models import Produtos
+from django.utils import timezone
 
 def index(request):
     todos_produtos = Produtos.objects.all()
@@ -12,12 +13,19 @@ def index(request):
 
     venda_total = todos_produtos.filter(status="Vendas totais")
 
+    hora_atual = timezone.now() 
+
+    mes_atual = hora_atual.month
+
+    vendas_mes = todos_produtos.filter(horario_venda__month=mes_atual)
+
     context = {"nome_pagina": "Seu estoque", 
                "todos_produtos": todos_produtos, 
                "produtos_estoque": produtos_estoque.count(), 
                "produtos_recebidos": produtos_recebidos.count(),
                "produtos_vendidos": produtos_vendidos.count(),
                "venda_total": venda_total.count(),
+               "vendas_mes": vendas_mes.count(),
                }
 
     return render(request, "index.html", context)
